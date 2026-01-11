54789a4bb45b8280661b060fa6b4bb35f4163c07
Roni Biswas <ronibiswas068@gmail.com>
2026-01-11 19:19:04 +0600

    implement get request API for fetching data from db

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -60,3 +60,19 @@ export async function POST(req: NextRequest) {
     );
   }
 }
+
+export async function GET() {
+  try {
+    await connectMongoDB();
+    const event = await Event.find().sort({ createdAt: -1 });
+    return NextResponse.json(
+      { message: "Events fetched successfully", event },
+      { status: 200 }
+    );
+  } catch (e) {
+    return NextResponse.json(
+      { message: "Events fetching failed!", error: e },
+      { status: 500 }
+    );
+  }
+}
